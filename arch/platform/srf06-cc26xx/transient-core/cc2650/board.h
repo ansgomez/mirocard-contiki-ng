4c6aeb6faf6c75f044bc0f68230494073bf656de
Lukas Sigrist <lukas.sigrist@tik.ee.ethz.ch>
2018-08-15 08:15:34 +0200

    update transient-core platform for transient BLE node hardware v2.0

--- arch/platform/srf06-cc26xx/transient-core/cc2650/board.h
+++ arch/platform/srf06-cc26xx/transient-core/cc2650/board.h
@@ -61,8 +61,8 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define LEDS_CONF_COUNT                 1
-#define LEDS_CONF_GREEN                 1
+#define LEDS_CONF_COUNT           1
+#define LEDS_CONF_GREEN           1
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -71,7 +71,7 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_LED_1          IOID_6
+#define BOARD_IOID_LED_1          IOID_11
 #define BOARD_LED_ALL             BOARD_IOID_LED_1
 /** @} */
 /*---------------------------------------------------------------------------*/
@@ -81,8 +81,7 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_KEY_USER       IOID_24
-#define BOARD_KEY_USER            (1 << BOARD_IOID_KEY_USER)
+#define BOARD_IOID_KEY_USER       IOID_10
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -93,6 +92,8 @@
  */
 #define BOARD_IOID_GPIO_1         IOID_12
 #define BOARD_IOID_GPIO_2         IOID_13
+#define BOARD_IOID_GPIO_3         IOID_14
+#define BOARD_IOID_GPIO_4         IOID_15
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -105,10 +106,6 @@
 #define BOARD_IOID_UART_TX        IOID_3
 #define BOARD_IOID_UART_RTS       IOID_UNUSED
 #define BOARD_IOID_UART_CTS       IOID_UNUSED
-#define BOARD_UART_RX             (1 << BOARD_IOID_UART_RX)
-#define BOARD_UART_TX             (1 << BOARD_IOID_UART_TX)
-#define BOARD_UART_RTS            (1 << BOARD_IOID_UART_RTS)
-#define BOARD_UART_CTS            (1 << BOARD_IOID_UART_CTS)
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -117,15 +114,15 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_EMU_VSEL_1     IOID_UNUSED
-#define BOARD_IOID_EMU_VSEL_2     IOID_UNUSED
-#define BOARD_IOID_EMU_VSEL_3     IOID_UNUSED
-#define BOARD_IOID_EMU_VSEL_4     IOID_UNUSED
-#define BOARD_IOID_EMU_ESEL_1     IOID_UNUSED
-#define BOARD_IOID_EMU_ESEL_2     IOID_UNUSED
-#define BOARD_IOID_EMU_TRIG       IOID_UNUSED
-#define BOARD_IOID_EMU_COMP       IOID_UNUSED
-#define BOARD_IOID_EMU_VBUF       IOID_UNUSED
+#define BOARD_IOID_EMU_VSEL_1     IOID_18
+#define BOARD_IOID_EMU_VSEL_2     IOID_19
+#define BOARD_IOID_EMU_VSEL_3     IOID_20
+#define BOARD_IOID_EMU_VSEL_4     IOID_21
+#define BOARD_IOID_EMU_ESEL_1     IOID_22
+#define BOARD_IOID_EMU_ESEL_2     IOID_23
+#define BOARD_IOID_EMU_TRIG       IOID_24
+#define BOARD_IOID_EMU_COMP       IOID_25
+#define BOARD_IOID_EMU_VBUF       IOID_26
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -134,10 +131,10 @@
  * @{
  */
 #define EXT_RTC_SPI_CONTROLLER    SPI_CONTROLLER_SPI0
-#define BOARD_IOID_RTC_SCK        IOID_21
-#define BOARD_IOID_RTC_MOSI       IOID_18
-#define BOARD_IOID_RTC_MISO       IOID_22
-#define BOARD_IOID_RTC_CS         IOID_20
+#define BOARD_IOID_RTC_SCK        IOID_4
+#define BOARD_IOID_RTC_MOSI       IOID_5
+#define BOARD_IOID_RTC_MISO       IOID_6
+#define BOARD_IOID_RTC_CS         IOID_7
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -146,8 +143,8 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_RTC_IRQ        IOID_19
-#define BOARD_IOID_RTC_CHARGE     IOID_14
+#define BOARD_IOID_RTC_CHARGE     IOID_8
+#define BOARD_IOID_RTC_IRQ        IOID_9
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -156,11 +153,11 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define EXT_FRAM_SPI_CONTROLLER   SPI_CONTROLLER_SPI0
-#define BOARD_IOID_FRAM_SCK       IOID_9
-#define BOARD_IOID_FRAM_MOSI      IOID_8
-#define BOARD_IOID_FRAM_MISO      IOID_11
-#define BOARD_IOID_FRAM_CS        IOID_10
+#define EXT_FRAM_SPI_CONTROLLER   SPI_CONTROLLER_SPI1
+#define BOARD_IOID_FRAM_SCK       IOID_27
+#define BOARD_IOID_FRAM_MOSI      IOID_28
+#define BOARD_IOID_FRAM_MISO      IOID_29
+#define BOARD_IOID_FRAM_CS        IOID_30
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -169,18 +166,9 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_I2C_SCL        IOID_4
-#define BOARD_IOID_I2C_SDA        IOID_5
+#define BOARD_IOID_I2C_SCL        IOID_0
+#define BOARD_IOID_I2C_SDA        IOID_1
 /** @} */
-/*---------------------------------------------------------------------------*/
-/**
- * \name SHT31 sensor IOID mappings
- *
- * PIN configuration for the use of external pins, to be used with the RocketLogger
- * @{
- */
-#define BOARD_IOID_SHT_RESET      IOID_6
-#define BOARD_IOID_SHT_ALERT      IOID_7
 /** @} */
 /*---------------------------------------------------------------------------*/
 /**
@@ -211,44 +199,11 @@
  * Those values are not meant to be modified by the user
  * @{
  */
-#define BOARD_IOID_DIO0           IOID_0
-#define BOARD_IOID_DIO1           IOID_1
-// #define BOARD_IOID_DIO2          IOID_2  // UART
-// #define BOARD_IOID_DIO3          IOID_3  // UART
-#define BOARD_IOID_DIO4           IOID_4
-#define BOARD_IOID_DIO5           IOID_5
-//#define BOARD_IOID_DIO6           IOID_6  //SHT_RESET
-//#define BOARD_IOID_DIO7           IOID_7  //SHT_ALERT
-//#define BOARD_IOID_FRAM_MISO      IOID_8  //FRAM
-//#define BOARD_IOID_FRAM_MOSI      IOID_9  //FRAM
-//#define BOARD_IOID_FRAM_SCK       IOID_10 //FRAM
-//#define BOARD_IOID_FRAM_CS        IOID_11 //FRAM
-//#define BOARD_IOID_DIO12          IOID_12 //GPIO12
-//#define BOARD_IOID_DIO13          IOID_13 //GPIO13
-//#define BOARD_IOID_DIO14          IOID_14 //RTC Charging
-#define BOARD_IOID_DIO15          IOID_15
 #define BOARD_IOID_TDO            IOID_16
 #define BOARD_IOID_TDI            IOID_17
-//#define BOARD_IOID_DIO18          IOID_18 //RTC_CS
-//#define BOARD_IOID_DIO19          IOID_19 //RTC_IRQ
-//#define BOARD_IOID_DIO20          IOID_20 //RTC_SCK
-//#define BOARD_IOID_DIO21          IOID_21 //RTC_MISO
-//#define BOARD_IOID_DIO22          IOID_22 //RTC_MOSI
-//#define BOARD_IOID_DIO23          IOID_23
-//#define BOARD_IOID_DIO24          IOID_24
-#define BOARD_IOID_DIO25          IOID_25
-#define BOARD_IOID_DIO26          IOID_26
-#define BOARD_IOID_DIO27          IOID_27
-#define BOARD_IOID_DIO28          IOID_28
-#define BOARD_IOID_DIO29          IOID_29
-#define BOARD_IOID_DIO30          IOID_30
 
 #define BOARD_UNUSED_PINS { \
-    BOARD_IOID_DIO0, BOARD_IOID_DIO1, \
-    BOARD_IOID_DIO15, \
     BOARD_IOID_TDO, BOARD_IOID_TDI, \
-    BOARD_IOID_DIO25, BOARD_IOID_DIO26, BOARD_IOID_DIO27, \
-    BOARD_IOID_DIO28, BOARD_IOID_DIO29, BOARD_IOID_DIO30, \
     IOID_UNUSED \
   }
 /** @} */
@@ -266,7 +221,7 @@
  * \name Device string used on startup
  * @{
  */
-#define BOARD_STRING "TI CC2650 Transient"
+#define BOARD_STRING "TI CC2650 Transient Core"
 
 /** @} */
 /*---------------------------------------------------------------------------*/
