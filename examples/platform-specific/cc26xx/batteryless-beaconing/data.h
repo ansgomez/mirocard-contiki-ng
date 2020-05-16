03e54546debccfcd64f5e5cfe4f416f7e6f009e4
Andres Gomez <gomez@miromico.ch>
2020-05-16 03:38:10 +0200

    adds initial joined beaconing project. Needs to be fully integrated and cleaned up.

---
+++ examples/platform-specific/cc26xx/batteryless-beaconing/data.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2019, Swiss Federal Institute of Technology (ETH Zurich)
+ * Copyright (c) 2020, Miromico AG
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+/*---------------------------------------------------------------------------*/
+#ifndef DATA_H_
+#define DATA_H_
+/*---------------------------------------------------------------------------*/
+#include <stdbool.h>
+#include <stdint.h>
+#include "sys/cc.h"
+
+#include "sense-transmit.h"
+/*---------------------------------------------------------------------------*/
+typedef struct batteryless_data_state {
+  uint16_t history_head_index;
+  uint16_t history_tail_index;
+  uint32_t aggregate_timestamp;
+  uint32_t average_timestamp;
+  int32_t average_sum[2];
+  uint16_t average_count;
+} batteryless_data_state_t;
+/*---------------------------------------------------------------------------*/
+typedef union batteryless_data_unit {
+  struct {
+    uint32_t time;
+    uint8_t data[3];
+  };
+  uint8_t bytes[7];
+} batteryless_data_unit_t CC_ALIGN (4);
+/*---------------------------------------------------------------------------*/
+/**
+ * Actual size of the unaligned transient data unit structure
+ */
+#define BATTERYLESS_DATA_UNIT_SIZE    7
+/**
+ * Buffer index of the temperature data
+ */
+#define DATA_TEMPERATURE_INDEX      0
+/**
+ * Buffer index of the humidity data
+ */
+#define DATA_HUMIDITY_INDEX         1
+/**
+ * Timeout for average building [in seconds]
+ */
+#define DATA_AVERAGE_TIMEOUT        360
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+
+/*---------------------------------------------------------------------------*/
+#endif /* DATA_H_ */
+/*---------------------------------------------------------------------------*/
